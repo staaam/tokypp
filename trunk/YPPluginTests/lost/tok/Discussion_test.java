eb4d40d9947fa410cf67a1d10c12c2e4e454b73b
michael.gelfand <michael.gelfand@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-01-20 16:55:11 +0000

    Initial import.

---
+++ trunk/YPPluginTests/lost/tok/Discussion_test.java
@@ -0,0 +1,423 @@
+package lost.tok;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.XPath;
+import org.dom4j.io.SAXReader;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import junit.framework.TestCase;
+
+public class Discussion_test extends TestCase {
+
+	/*
+	 * Shay: I've made an attempt to clean all the files on exit, but it doesn't
+	 * work public void tearDown() { IWorkspace ws =
+	 * ResourcesPlugin.getWorkspace(); String wsRoot =
+	 * ws.getRoot().getLocation().toOSString(); File f = new File(wsRoot); for
+	 * (String s : f.list()) { deleteFileOrFolder(new File(s)); } }
+	 * 
+	 * private void deleteFileOrFolder(File file) { if (file.isDirectory()) {
+	 * for (String s : file.list()) { deleteFileOrFolder(new File(s)); } }
+	 * file.deleteOnExit(); }
+	 */
+	private void creation(ToK tok, String projectName) throws CoreException,
+			IOException {
+		tok.createToKProject(projectName, "Guy", Paths.SOURCE_EXAMPLE);
+	}
+
+	// test that the add discussion work
+	public void testPre() throws CoreException, IOException {
+		ToK tok = new ToK();
+		Discussion disc;
+		creation(tok, "test0");
+		tok.addDiscussion("test");
+		disc = tok.getDiscussion("test");
+		assertTrue(disc.getDiscName() == "test");
+	}
+
+	// test addOpinion
+	public void testAddOpinion1() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testAO1");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper
+				.createXPath("//opinion[name='one']");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 1);
+	}
+
+	// test that you cant add 2 opinions with the same name
+	public void testAddOpinion2() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testAO2");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("Default Opinion");
+		disc.addOpinion("one");
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//opinion");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 2);
+	}
+
+	// test removeOpinion
+	public void testRemoveOpinion1() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRO1");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("two");
+		disc.removeOpinion(2);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//opinion");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 2);
+	}
+
+	// chack that you cant remove the defult opinion, and removing of non
+	// existiong opinions
+	public void testRemoveOpinion2() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRO2");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.removeOpinion(1);
+		disc.removeOpinion(3);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//opinion");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 2);
+	}
+
+	// test relocateQuote
+	public void testRelocateQuote1() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRQ1");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addQuoteTest(1, 1);
+		disc.relocateQuote(1, 2);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector1 = DocumentHelper
+				.createXPath("//opinion[name='one']/quote");
+		List result1 = xpathSelector1.selectNodes(doc);
+		XPath xpathSelector2 = DocumentHelper
+				.createXPath("//opinion[id='1']/quote");
+		List result2 = xpathSelector2.selectNodes(doc);
+		assertTrue(result1.size() == 1 && result2.size() == 0);
+	}
+
+	// test relocateQuote, bad quote id or target id
+	public void testRelocateQuote2() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRQ2");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addQuoteTest(1, 1);
+		disc.relocateQuote(1, 7);
+		disc.relocateQuote(7, 2);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector1 = DocumentHelper
+				.createXPath("//opinion[name='one']/quote");
+		List result1 = xpathSelector1.selectNodes(doc);
+		XPath xpathSelector2 = DocumentHelper
+				.createXPath("//opinion[id='1']/quote");
+		List result2 = xpathSelector2.selectNodes(doc);
+		assertTrue(result1.size() == 0 && result2.size() == 1);
+	}
+
+	// test removeQuote
+	public void testRemoveQuote1() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRMQ1");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addQuoteTest(1, 1);
+		disc.removeQuote(1);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//quote");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.isEmpty());
+	}
+
+	// test removeQuote, Quote doesnt exist
+	public void testRemoveQuote2() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRMQ2");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addQuoteTest(1, 1);
+		disc.removeQuote(7);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//quote");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 1);
+	}
+
+	// test createOpinionLink
+	public void testCreateOpinionLink1() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testCOL1");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("two");
+		disc.addOpinion("three");
+		disc.createOpinionLink(2, 3, "link between 'one' and 'two'",
+				"opposition");
+		disc.createOpinionLink(2, 4, "link between 'one' and 'three'",
+				"opposition");
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector1 = DocumentHelper
+				.createXPath("//opinion[id='2']/opinionRel");
+		List result1 = xpathSelector1.selectNodes(doc);
+		XPath xpathSelector2 = DocumentHelper
+				.createXPath("//opinion[id='3']/opinionRel[targetId='2']");
+		List result2 = xpathSelector2.selectNodes(doc);
+		XPath xpathSelector3 = DocumentHelper
+				.createXPath("//opinion[id='4']/opinionRel[targetId='2']");
+		List result3 = xpathSelector3.selectNodes(doc);
+		assertTrue(result1.size() == 2 && result2.size() == 1
+				&& result3.size() == 1);
+	}
+
+	// test createOpinionLink, link already exist and link between opinion that
+	// dont exist
+	public void testCreateOpinionLink2() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testCOL2");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("two");
+		disc.createOpinionLink(2, 3, "good link", "opposition");
+		disc.createOpinionLink(2, 3, "cant link (already exist)", "opposition");
+		disc.createOpinionLink(2, 7, "cant link (opinion doesnt exist)",
+				"opposition");
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//opinionRel");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 2);
+	}
+
+	// test removeOpinionLink
+	public void testRemoveOpinionLink1() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testROL1");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("two");
+		disc.addOpinion("three");
+		disc.createOpinionLink(2, 3, "link between 'one' and 'two'",
+				"opposition");
+		disc.createOpinionLink(2, 4, "link between 'one' and 'three'",
+				"opposition");
+		disc.removeOpinionLink(4, 2);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//opinionRel");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 2);
+	}
+
+	// test removeOpinionLink, removing a link that doesnt exist
+	public void testRemoveOpinionLink2() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testROL2");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("two");
+		disc.addOpinion("three");
+		disc.createOpinionLink(2, 3, "link between 'one' and 'two'",
+				"opposition");
+		disc.createOpinionLink(2, 4, "link between 'one' and 'three'",
+				"opposition");
+		disc.removeOpinionLink(2, 7);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector = DocumentHelper.createXPath("//opinionRel");
+		List result = xpathSelector.selectNodes(doc);
+		assertTrue(result.size() == 4);
+	}
+
+	// test removeOpinion, opinion with quotes and relations
+	public void testRemoveOpinion3() throws CoreException, IOException {
+		ToK tok = new ToK();
+		creation(tok, "testRO3");
+		tok.addDiscussion("test");
+		Discussion disc = tok.getDiscussion("test");
+		disc.addOpinion("one");
+		disc.addOpinion("two");
+		disc.addOpinion("three");
+		disc.createOpinionLink(2, 3, "should be removed", "opposition");
+		disc.createOpinionLink(2, 4, "should be removed", "opposition");
+		disc.createOpinionLink(3, 4, "shouldn't be removed", "opposition");
+		disc.addQuoteTest(1, 2);
+		disc.addQuoteTest(2, 2);
+		disc.removeOpinion(2);
+
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		String path = disc.getMyToK().getDiscussionFolder().getFile(
+				disc.getDiscName() + ".dis").getLocation().toOSString();
+		File file = new File(path);
+		try {
+			doc = reader.read(file);
+		} catch (DocumentException e) {
+			e.printStackTrace();
+		}
+
+		XPath xpathSelector1 = DocumentHelper.createXPath("//opinionRel");
+		List result1 = xpathSelector1.selectNodes(doc);
+		XPath xpathSelector2 = DocumentHelper.createXPath("//quote");
+		List result2 = xpathSelector2.selectNodes(doc);
+		assertTrue(result1.size() == 2 && result2.size() == 2);
+	}
+
+}
---
+++ trunk/YPPluginTests/lost/tok/ToK_test.java
@@ -0,0 +1,404 @@
+package lost.tok;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.TestCase;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.QualifiedName;
+
+public class ToK_test extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		// ResourcesPlugin.getWorkspace().
+		// delete((IResource[])ResourcesPlugin.getWorkspace().getRoot().getProjects(),true,
+		// null);
+		// IProgressMonitor progMonitor = null;
+		// IProject[] projects =
+		// ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		//
+		// for (int i = projects.length-1; i >=0; i--) {
+		// IProject project = projects[i];
+		// project.delete(true, progMonitor);
+		// //String path = project.getLocation().toOSString();
+		// //File file = new File(path);
+		// //recursiveDelete(file);
+		// }
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		// IProject[] projects = ResourcesPlugin.getWorkspace().getRoot()
+		// .getProjects();
+		//
+		// for (int i = 0; i < projects.length; i++) {
+		// IProject project = projects[i];
+		// String path = project.getLocation().toOSString();
+		// File file = new File(path);
+		// recursiveDelete(file);
+		// }
+		// IProgressMonitor progMonitor = null;
+		// IProject[] projectss =
+		// ResourcesPlugin.getWorkspace().getRoot().getProjects();
+		//
+		// if (projectss.length>0){
+		// for (int i = projects.length-1; i >=0; i--) {
+		// IProject project = projects[i];
+		// project.delete(true, progMonitor);
+		// //String path = project.getLocation().toOSString();
+		// //File file = new File(path);
+		// //recursiveDelete(file);
+		// }
+		// }
+		// ResourcesPlugin.getWorkspace().getRoot().refreshLocal(10, null);
+		//		
+		// System.out.println("Finished Deleting!");
+	}
+
+	// basic project creation test
+	public void testCreateProject() throws CoreException, IOException {
+
+		ToK tok = new ToK();
+		int numProjectsBefore = ResourcesPlugin.getWorkspace().getRoot()
+				.getProjects().length;
+		tok
+				.createToKProject("michalsProj_1", "michalzim",
+						Paths.SOURCE_EXAMPLE);
+
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProjects().length == numProjectsBefore + 1);
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_1").exists());
+	}
+
+	// tests to see if the project parameters have been set correctly
+	public void testProjectProperties() throws CoreException, IOException {
+
+		ToK tok = new ToK();
+		tok
+				.createToKProject("michalsProj_2", "michalzim",
+						Paths.SOURCE_EXAMPLE);
+
+		// check project name
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_2").getName().equals("michalsProj_2"));
+
+		// check creator name
+		QualifiedName name = new QualifiedName("TOK Project creator", "Creator");
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_2").getPersistentProperty(name).compareTo(
+				"michalzim") == 0);
+
+		// check root name
+		name = new QualifiedName("TOK Project root", "Root");
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_2").getPersistentProperty(name).compareTo(
+				"source_example.src") == 0);
+
+	}
+
+	// ILLEGAL project creation test
+	public void testIllegalCreateProject() throws CoreException, IOException {
+
+		ToK tok = new ToK();
+
+		// root file not an src file
+		int dotLoc = Paths.SOURCE_EXAMPLE.lastIndexOf('.');
+		String noExt = Paths.SOURCE_EXAMPLE.substring(0, dotLoc - 1);
+		String falseExt = noExt + ".xml";
+
+		tok.createToKProject("michalsProj_3", "michalzim", falseExt);
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_3").getFolder("Sources").getFile(falseExt)
+				.exists() == false);
+
+		// root file not existant
+		tok.createToKProject("michalsProj_99", "michalzim",
+				Paths.SOURCE_EXAMPLE + "aaa");
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_99").getFolder("Sources").getFile(
+				Paths.SOURCE_EXAMPLE + "aaa").exists() == false);
+
+		// illegal project name
+		try {
+			tok.createToKProject("**michalsProj", "michalzim",
+					Paths.SOURCE_EXAMPLE);
+		} catch (CoreException e) {
+			return;
+		}
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"michalsProj_99").exists() == false);
+	}
+
+	// testing that the two project Folders were created
+	public void testProjectFolders() throws CoreException, IOException {
+
+		ToK tok = new ToK();
+		tok
+				.createToKProject("michalsProj_4", "michalzim",
+						Paths.SOURCE_EXAMPLE);
+
+		// getting the handles to the folders
+		IFolder srcFolder = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject("michalsProj_4").getFolder("Sources");
+		IFolder discFolder = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject("michalsProj_4").getFolder("Discussions");
+
+		// checking that the folders exist
+		assertTrue(srcFolder.exists() == true);
+		assertTrue(discFolder.exists() == true);
+
+	}
+
+	// testing that the project files were created
+	public void testProjectFiles() throws CoreException, IOException {
+
+		ToK tok = new ToK();
+		tok
+				.createToKProject("michalsProj_5", "michalzim",
+						Paths.SOURCE_EXAMPLE);
+
+		// getting the handles to the files
+		// Authors file
+		File authorsFile = new File(ResourcesPlugin.getWorkspace().getRoot()
+				.getProject("michalsProj_5").getLocationURI().getPath()
+				+ "/Authors.xml");
+		// Links file
+		File linksFile = new File(ResourcesPlugin.getWorkspace().getRoot()
+				.getProject("michalsProj_5").getLocationURI().getPath()
+				+ "/Links.xml");
+
+		// checking that the files exist
+		assertTrue(authorsFile.exists());
+		assertTrue(linksFile.exists());
+
+	}
+
+	// testing that the project root were created correctly
+	public void testProjectRoot() throws CoreException, IOException {
+
+		ToK tok = new ToK();
+		tok
+				.createToKProject("michalsProj_6", "michalzim",
+						Paths.SOURCE_EXAMPLE);
+
+		// Root file
+		IFolder srcFolder = ResourcesPlugin.getWorkspace().getRoot()
+				.getProject("michalsProj_6").getFolder("Sources");
+		IFile rootFile = srcFolder.getFile("source_example.src");
+
+		// checking that the root exists
+		assertTrue(rootFile.exists());
+
+		// checking that it is marked as root
+		QualifiedName name = new QualifiedName("TOK Root File", "Is Root");
+		// the assert will return true if it is marked as root
+		assertTrue(rootFile.getPersistentProperty(name) == "true");
+
+	}
+
+	public void testAddDiscussion() throws CoreException, IOException {
+		ToK tok = new ToK();
+		tok.createToKProject("AriesProj1", "Arie", Paths.SOURCE_EXAMPLE);
+		tok.addDiscussion("test");
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"AriesProj1").getFolder("Discussions").getFile("test.dis") != null);
+	}
+
+	public void testLinkDiscussionRoot() throws CoreException, IOException {
+		ToK tok = new ToK();
+		tok.createToKProject("AriesProj2", "Arie", Paths.SOURCE_EXAMPLE);
+		tok.addDiscussion("test");
+		tok.linkDiscussionRoot(tok.getDiscussion("test"), new Excerption(
+				"/Bible/The Begining/The Continue/The First Paragraph", "", 2,
+				22));
+
+		IFile links = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"AriesProj2").getFile("Links.xml");
+		String path = links.getLocation().toOSString();
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		try {
+			doc = reader.read(new File(path));
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Node link = doc
+				.selectSingleNode("//link/discussionFile[text()=\"test.dis\"]");
+		assertTrue(link != null);
+	}
+
+	public void testNewLinkDiscussionRoot() throws CoreException, IOException {
+		ToK tok = new ToK();
+		tok.createToKProject("AriesProj3", "Arie", Paths.SOURCE_EXAMPLE);
+		tok.linkNewDiscussionRoot("New Discussion", new Excerption(
+				"/Bible/The Begining/The Continue/The First Paragraph", "", 2,
+				22));
+
+		assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"AriesProj3").getFolder("Discussions").getFile("test.dis") != null);
+		IFile links = ResourcesPlugin.getWorkspace().getRoot().getProject(
+				"AriesProj3").getFile("Links.xml");
+		Document doc = DocumentHelper.createDocument();
+		SAXReader reader = new SAXReader();
+		try {
+			doc = reader.read(new File(links.getLocation().toOSString()));
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Node link = doc
+				.selectSingleNode("//link/discussionFile[text()=\"New Discussion.dis\"]");
+		assertTrue(link != null);
+	}
+
+	private void recursiveDelete(File dirPath) {
+		String[] ls = dirPath.list();
+
+		for (int idx = 0; idx < ls.length; idx++) {
+			File file = new File(dirPath, ls[idx]);
+			if (file.isDirectory())
+				recursiveDelete(file);
+			file.delete();
+		}
+	}
+
+	//Evgeni
+	//Testing valid source with invalid path
+	public void testValidSourceInvalidPath() throws IOException
+	{	
+		System.out.println("\nTest Valid Source Invalid Path \n");
+		ToK tok = new ToK();
+		try {
+			tok.createToKProject("Evgeni_project", "Evgeni", Paths.BASE + "1" + "Bavel_en.src" );			
+		} 
+		catch (Exception e){
+			System.out.println("Failed to create a project");
+			return;
+		}
+
+		String sourceFilePath = "C:\\Temp\\Bad_Path\\Rashi_en.src";
+		tok.addSource(sourceFilePath);		
+
+		assertTrue(true);
+	}
+
+	//Evgeni
+	//Testing invalid sources valid path
+	public void testInvalidSourceValidPath() throws IOException
+	{		
+		System.out.println("\nTest Invalid Source Valid Path\n");
+		ToK tok = new ToK();
+		try {
+			tok.createToKProject("Evgeni_project", "Evgeni", Paths.BASE + "2" + "Bavel_en.src" );
+		} 
+		catch (Exception e){
+			System.out.println("Failed to create a project");
+			return;
+		}
+
+		String sourceFilePath = Paths.BASE + "Rashi_no_tag.src";
+		tok.addSource(sourceFilePath);		
+
+		assertTrue(true);
+	}
+	
+	//Evgeni
+	//Testing valid new source
+	public void testValidSourceValidity() throws IOException
+	{		
+		System.out.println("\nTest Valid Source Validity\n");
+		ToK tok = new ToK();
+		try {
+			tok.createToKProject("Evgeni_project", "Evgeni", Paths.BASE + "4" + "Bavel_en.src" );
+		} 
+		catch (Exception e){
+			System.out.println("Failed to create a project");
+			return;
+		}
+
+		String sourceFilePath = Paths.BASE + "2" + "Rashi_en.src";
+		tok.addSource(sourceFilePath);		
+	
+		assertTrue(true);
+	}
+	
+	//Evgeni
+	//Testing addition of existing source
+	public void testSourceExists() throws IOException
+	{		
+		System.out.println("\nTest Source Exists\n");
+		ToK tok = new ToK();
+		try {
+			tok.createToKProject("Evgeni_project", "Evgeni", Paths.BASE + "5" + "Bavel_en.src" );
+		} 
+		catch (Exception e){
+			System.out.println("Failed to create a project");
+			return;
+		}
+
+		String sourceFilePath = Paths.BASE + "3" + "Rashi_en.src";
+		tok.addSource(sourceFilePath);
+		tok.addSource(sourceFilePath);
+
+		assertTrue(true);
+	}
+
+	
+	//Evgeni
+	//Testing addition of source which author already exists in project
+	public void testAuthorExists() throws IOException
+	{		
+		System.out.println("\nTest Author Exists\n");
+		ToK tok = new ToK();
+		try {
+			tok.createToKProject("Evgeni_project", "Evgeni", Paths.BASE + "6" + "Bavel_en.src" );
+		} 
+		catch (Exception e){
+			System.out.println("Failed to create a project");
+			return;
+		}
+
+		String sourceFilePath_a = Paths.BASE + "4" + "Rashi_en.src";
+		tok.addSource(sourceFilePath_a);
+		String sourceFilePath_b = Paths.BASE + "5" + "Rashi_en.src";
+		tok.addSource(sourceFilePath_b);
+
+		assertTrue(true);
+	}
+	
+	//Evgeni
+	//Testing changing authors rank
+	public void testChangeAuthorRank() throws IOException
+	{		
+		System.out.println("\nTest Change Author Rank\n");
+		ToK tok = new ToK();
+		try {
+			tok.createToKProject("Evgeni_project", "Evgeni", Paths.BASE + "6" + "Bavel_en.src" );
+		} 
+		catch (Exception e){
+			System.out.println("Failed to create a project");
+			return;
+		}
+
+		String sourceFilePath = Paths.BASE + "4" + "Rashi_en.src";
+		tok.addSource(sourceFilePath);
+		Author author = new Author();
+		author.name = "Rashi";
+		author.rank = 3;
+		tok.RankChangeByUser(author);
+
+		assertTrue(true);
+	}
+
+}
