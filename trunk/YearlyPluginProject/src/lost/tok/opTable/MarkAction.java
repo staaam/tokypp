1c628f1d670c02dd5387c1af2f334637aca67ffd
michalzim <michalzim@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-04-17 22:04:20 +0000

    begin editors union

--- trunk/YearlyPluginProject/src/lost/tok/opTable/MarkAction.java
+++ trunk/YearlyPluginProject/src/lost/tok/opTable/MarkAction.java
@@ -1,6 +1,16 @@
 package lost.tok.opTable;
 
+import java.util.List;
+
+import lost.tok.Excerption;
+import lost.tok.excerptionsView.ExcerptionView;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.part.FileEditorInput;
 
 public class MarkAction extends AbstractEditorAction {
 
@@ -12,6 +22,30 @@ public class MarkAction extends AbstractEditorAction {
 		}
 
 		((OperationTable) activeEditor).mark(currentSelection);
+		
+		updateExcerptionView();
 	}
 
+	private void updateExcerptionView() {
+		assert (activeEditor != null);
+		if (currentSelection == null || currentSelection.isEmpty()
+				|| currentSelection.getLength() == 0) {
+			return;
+		}
+
+		OperationTable opTable = (OperationTable) activeEditor;
+		
+
+		FileEditorInput fileEditorInput = 
+			(FileEditorInput)opTable.getEditorInput();
+		IFile file = fileEditorInput.getFile();
+		String fileName = file.getName();
+		
+		List<Excerption> excerptions = opTable.getMarked();
+		Excerption[] exps = new Excerption[excerptions.size()];
+		excerptions.toArray(exps);
+
+		ExcerptionView.getView().setExcerptions(fileName, exps, file);
+		//expViewer.setFocus();
+	}
 }
--- trunk/YearlyPluginProject/src/lost/tok/opTable/UnMarkAllAction.java
+++ trunk/YearlyPluginProject/src/lost/tok/opTable/UnMarkAllAction.java
@@ -1,11 +1,14 @@
 package lost.tok.opTable;
 
+import lost.tok.excerptionsView.ExcerptionView;
+
 import org.eclipse.jface.action.IAction;
 
 public class UnMarkAllAction extends AbstractEditorAction {
 
 	public void run(IAction action) {
 		((OperationTable) activeEditor).clearMarked();
+		ExcerptionView.getView().clear();
 	}
 
 }
