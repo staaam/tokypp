cabb7e464d0f7b37e59c84932d7cae04477cf302
michalzim <michalzim@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-04-26 20:22:02 +0000

    Type:  New Feature What: perspective Description: now the perspective gets loaded

--- trunk/YearlyPluginProject/src/lost/tok/perspective/Perspective.java
+++ trunk/YearlyPluginProject/src/lost/tok/perspective/Perspective.java
@@ -12,13 +12,25 @@ import org.eclipse.ui.IPerspectiveFactory;
  *
  */
 public class Perspective implements IPerspectiveFactory{
+	
+	public interface IPerspectivePlugin {
+        /**
+         * Plugin id.
+         */
+        public final static String PLUGIN_ID = "lost.tok.perspective";
+        
+        /**
+         * Test perspective id.
+         */
+        public final static String TEST_PERSPECTIVE_ID = PLUGIN_ID + ".ToK";
+	}
 
 	/**
 	 * The createInitialLayout method is called on the IPerspectiveFactory. 
 	 *  This method defines the initial layout for a page 
 	 */
 	public void createInitialLayout(IPageLayout layout) {
-		System.out.println("bla1");
+		System.out.println("bla0");
 		defineActions(layout);
 	    defineLayout(layout);
 
@@ -29,7 +41,7 @@ public class Perspective implements IPerspectiveFactory{
 		//Editors are placed for free.
         String editorArea = layout.getEditorArea();
 
-        System.out.println("bla2");
+        System.out.println("bla1");
         // Place navigator and outline to left of
         // editor area.
         IFolderLayout left =
@@ -37,10 +49,12 @@ public class Perspective implements IPerspectiveFactory{
         left.addView(IPageLayout.ID_RES_NAV);
         left.addView(IPageLayout.ID_OUTLINE);
 
-		
+        IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, (float) 0.26, editorArea);
+        bottom.addView(IPageLayout.ID_TASK_LIST);
 	}
 
 	private void defineActions(IPageLayout layout) {
+		System.out.println("bla2");
 		//Add "new wizards".
         layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");
         layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");
