8b61e24d30074358fc8417b0ae0e3818bdd419cf
michael.gelfand <michael.gelfand@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-04-29 18:57:23 +0000

    Type: Improvement What: Task 19.2 - New Discussion wizard checks that discussion not already exists Description: New Discussion wizard checks that discussion not already exists.  + Small bug fix with probably wrong project finding ExcerptionView small cleanups

--- trunk/YearlyPluginProject/src/lost/tok/opTable/actions/LinkDiscussionAction.java
+++ trunk/YearlyPluginProject/src/lost/tok/opTable/actions/LinkDiscussionAction.java
@@ -1,15 +1,8 @@
 package lost.tok.opTable.actions;
 
-import lost.tok.Messages;
 import lost.tok.excerptionsView.ExcerptionView;
-import lost.tok.opTable.wizards.NewLinkWizard;
 
 import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.part.FileEditorInput;
 
 /**
  * The action of linking a discussion to the currently marked text
@@ -18,17 +11,6 @@ public class LinkDiscussionAction extends AbstractEditorAction {
 	public void run(IAction action) {
 		assert (activeEditor != null);
 
-		IEditorInput editorInput = activeEditor.getEditorInput();		
-		if (editorInput instanceof FileEditorInput) {
-			NewLinkWizard wizard = new NewLinkWizard();
-			wizard.init(PlatformUI.getWorkbench(),
-					ExcerptionView.getView().getRoots());
-			WizardDialog dialog = new WizardDialog(new Shell(), wizard);
-			dialog.setTitle(Messages.getString("ExcerptionView.7")); //$NON-NLS-1$
-			dialog.updateSize();
-			dialog.create();
-			dialog.open();
-		}
-		//ExcerptionView.getView().linkDiscussionAction()
+		ExcerptionView.getView().linkDiscussion();
 	}
 }
--- trunk/YearlyPluginProject/src/lost/tok/wizards/NewDiscussion.java
+++ trunk/YearlyPluginProject/src/lost/tok/wizards/NewDiscussion.java
@@ -5,7 +5,6 @@ import lost.tok.ToK;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
@@ -13,10 +12,6 @@ import org.eclipse.ui.IWorkbench;
 
 public class NewDiscussion extends Wizard implements INewWizard {
 	private NewDiscussionPage page;
-
-	private ISelection selection;
-
-	private ToK tok;
 
 	private IProject project = null;
 
@@ -29,12 +24,16 @@ public class NewDiscussion extends Wizard implements INewWizard {
 		setWindowTitle(Messages.getString("NewDiscussionWizard.0")); //$NON-NLS-1$
 	}
 
+	public NewDiscussion(IProject project) {
+		this();
+		setProject(project);
+	}
 	/**
 	 * Adding the page to the wizard.
 	 */
 
 	public void addPages() {
-		page = new NewDiscussionPage(selection);
+		page = new NewDiscussionPage();
 		addPage(page);
 	}
 
@@ -43,35 +42,30 @@ public class NewDiscussion extends Wizard implements INewWizard {
 	 * will create an operation and run it using wizard as execution context.
 	 */
 	public boolean performFinish() {
-		tok = getToK(selection);
-		tok.addDiscussion(page.updateDiscussionName());
+		ToK.getProjectToK(project).addDiscussion(page.getDiscussionName());
 		return true;
 	}
 
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
-		this.selection = selection;
+		if (project != null) return;
+		
+		if (selection == null || 
+			selection.isEmpty() ||
+			selection.size() > 1) return;
+
+		Object obj = selection.getFirstElement();
+		if (obj instanceof IResource) {
+			IResource resource = (IResource) obj;
+			project = resource.getProject();
+		}
 	}
 
-	public void setProject(IProject project) {
+	private void setProject(IProject project) {
 		this.project = project;
 	}
-
-	private ToK getToK(ISelection selection) {
-		if (project == null) {
-			if (selection != null && selection.isEmpty() == false
-					&& selection instanceof IStructuredSelection) {
-				IStructuredSelection ssel = (IStructuredSelection) selection;
-				if (ssel.size() > 1) {
-					return null;
-				}
-				Object obj = ssel.getFirstElement();
-				if (obj instanceof IResource) {
-					IResource resource = (IResource) obj;
-					project = resource.getProject();
-				}
-			}
-		}
-		return ToK.getProjectToK(project);
+	
+	public IProject getProject() {
+		return project;
 	}
 	
 	public String getDiscussionName() {
