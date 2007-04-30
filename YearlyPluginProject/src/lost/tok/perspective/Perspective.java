1020316957b97de735282288a37ff22e230191d1
michalzim <michalzim@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-04-30 14:49:18 +0000

    Type:  New Feature What: perspective Description: now the perspective gets loaded with the needed shortcuts, views...

--- YearlyPluginProject/src/lost/tok/perspective/Perspective.java
+++ YearlyPluginProject/src/lost/tok/perspective/Perspective.java
@@ -2,6 +2,8 @@
  * 
  */
 package lost.tok.perspective;
+import lost.tok.excerptionsView.ExcerptionView;
+
 import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory; 
@@ -41,30 +43,38 @@ public class Perspective implements IPerspectiveFactory{
 		//Editors are placed for free.
         String editorArea = layout.getEditorArea();
 
-        System.out.println("bla1");
         // Place navigator and outline to left of
         // editor area.
         IFolderLayout left =
                 layout.createFolder("left", IPageLayout.LEFT, (float) 0.26, editorArea);
-        left.addView(IPageLayout.ID_RES_NAV);
-        left.addView(IPageLayout.ID_OUTLINE);
-
-        IFolderLayout bottom = layout.createFolder("bottom", IPageLayout.BOTTOM, (float) 0.26, editorArea);
-        bottom.addView(IPageLayout.ID_TASK_LIST);
+        left.addView("org.eclipse.jdt.ui.PackageExplorer");
+   
+        IFolderLayout bottom = 
+        	layout.createFolder("bottom", IPageLayout.BOTTOM, (float) 0.75, editorArea);
+        bottom.addView(ExcerptionView.ID);
 	}
 
 	private void defineActions(IPageLayout layout) {
 		System.out.println("bla2");
 		//Add "new wizards".
-        layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");
+		layout.addNewWizardShortcut("lost.tok.newTokWizard");
+		layout.addNewWizardShortcut("lost.tok.newDiscussionWizard.NewDiscussionWizard");
+		layout.addNewWizardShortcut("lost.tok.newRelationWizard.NewRelationWizard");
+		layout.addNewWizardShortcut("lost.tok.newLinkWizard.NewLinkWizard");
+		layout.addNewWizardShortcut("lost.tok.unparsedDocWizard");
+	
+		layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");
         layout.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");
         
+        
         // Add "show views".
+        layout.addShowViewShortcut(ExcerptionView.ID);
         layout.addShowViewShortcut(IPageLayout.ID_RES_NAV);
-        layout.addShowViewShortcut(IPageLayout.ID_BOOKMARKS);
-        layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
         layout.addShowViewShortcut(IPageLayout.ID_PROP_SHEET);
         layout.addShowViewShortcut(IPageLayout.ID_TASK_LIST);
+        layout.addShowViewShortcut(IPageLayout.ID_BOOKMARKS);
+        layout.addShowViewShortcut(IPageLayout.ID_EDITOR_AREA);
+        layout.addShowViewShortcut("org.eclipse.jdt.ui.PackageExplorer");
 	
 	}
 	
