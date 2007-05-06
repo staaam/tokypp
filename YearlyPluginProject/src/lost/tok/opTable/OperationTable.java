b20a703d2573a3925a894205a32795f1510fc164
arie.teter <arie.teter@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-05-06 00:15:44 +0000

    new feature

--- YearlyPluginProject/src/lost/tok/opTable/OperationTable.java
+++ YearlyPluginProject/src/lost/tok/opTable/OperationTable.java
@@ -9,13 +9,11 @@ import java.util.Map.Entry;
 
 import lost.tok.Excerption;
 import lost.tok.excerptionsView.ExcerptionView;
-import lost.tok.opTable.actions.MarkAction;
 import lost.tok.sourceDocument.Chapter;
 import lost.tok.sourceDocument.ChapterText;
 import lost.tok.sourceDocument.SourceDocument;
 
 import org.eclipse.core.resources.IProject;
-import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextSelection;
@@ -28,6 +26,7 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.part.FileEditorInput;
 
+// TODO: Auto-generated Javadoc
 /**
  * The operation table is an extension of the Eclipse editor. It displays source
  * files. It allows the user to mark excerptions in it. It DOESN'T allow the
@@ -35,24 +34,33 @@ import org.eclipse.ui.part.FileEditorInput;
  * 
  * @author Team LOST
  * @version 0.2
- * 
  */
 public class OperationTable extends TextEditor {
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.texteditor.AbstractTextEditor#setFocus()
+	 */
 	@Override
 	public void setFocus() {
 		super.setFocus();
 		updateExcerptionView();
 	}
-
-	/** The ID of the operation table editor */
+	
+	/** The ID of the operation table editor. */
 	public static final String EDITOR_ID = "lost.tok.opTable.OperationTable";
+	 
+	/** The Constant EDITOR_CONTEXT. */
+	public static final String EDITOR_CONTEXT = "lost.tok.opTable.PopUpMenu.Menu";
+	
+	/** The root discussions. */
 	private RootDiscussionsPart rootDiscussions = null;
+	
+	/** The root discussions view. */
 	private boolean rootDiscussionsView = false;
 
 	/**
 	 * Creates a new instance of the operation table Initialize it with the
-	 * SourceDocumentProvider we wrote
+	 * SourceDocumentProvider we wrote.
 	 */
 	public OperationTable() {
 		super();
@@ -63,8 +71,16 @@ public class OperationTable extends TextEditor {
 		markedText = new TreeMap<Integer, Integer>();
 	}
 
+	/* (non-Javadoc)
+	 * @see org.eclipse.ui.editors.text.TextEditor#initializeEditor()
+	 */
+	protected void initializeEditor() {
+        super.initializeEditor();
+        setEditorContextMenuId(EDITOR_CONTEXT);
+	}
+	
 	/**
-	 * Makes the docuemnt uneditable
+	 * Makes the docuemnt uneditable.
 	 * 
 	 * @return false
 	 */
@@ -85,37 +101,33 @@ public class OperationTable extends TextEditor {
 	public void createPartControl(Composite parent) {
 		super.createPartControl(parent);
 		showChangeInformation(false);
-
 		ISourceViewer srcview = getSourceViewer();
 		assert (srcview != null);
-
-		hookContextMenu(parent);
+		//hookContextMenu(parent);
 		getSourceViewerDecorationSupport(srcview).dispose();
-
 		refreshDisplay();
 	}
 
-	// Shay: this should add the pop up action, but it doesn't work :(
-	public void editorContextMenuAboutToShow(IMenuManager menu) {
-		super.editorContextMenuAboutToShow(menu);
-		addAction(menu, MarkAction.ACTION_ID);
-	}
-
 	// Shay: this should add the pop up action, but it doesn't work
+	/**
+	 * Hook context menu.
+	 * 
+	 * @param parent the parent
+	 */
 	protected void hookContextMenu(Control parent) {
 		if (rootDiscussionsView ) {
 			return;
 		}
 
-		MenuManager menuMgr = new MenuManager("#PopupMenu"); //$NON-NLS-1$
-		addAction(menuMgr, MarkAction.ACTION_ID);
-		menuMgr.setRemoveAllWhenShown(false);
+		MenuManager menuMgr = new MenuManager(EDITOR_CONTEXT,EDITOR_CONTEXT); //$NON-NLS-1$
+		menuMgr.setRemoveAllWhenShown(true);
+		menuMgr.removeAll();
 		Menu menu = menuMgr.createContextMenu(parent);
 		parent.setMenu(menu);
-
-		getSite().registerContextMenu(menuMgr, getSelectionProvider());
+		setEditorContextMenuId(EDITOR_CONTEXT);
+		getEditorSite().registerContextMenu(EDITOR_CONTEXT,menuMgr,getSelectionProvider());
 	}
-
+	
 	/*
 	 * Canceled attribute! /** These excerptions contain all the displayed text
 	 * (including the marked one) When the user cuts some text from the middle
@@ -124,20 +136,15 @@ public class OperationTable extends TextEditor {
 	 * List<Excerption> displayedText;
 	 */
 
-	/**
-	 * These excerptions contain all the marked text. The dictionary is the
-	 * function: offset->Excerption
-	 */
+	/** These excerptions contain all the marked text. The dictionary is the function: offset->Excerption */
 	SortedMap<Integer, Excerption> markedExcerptions;
 
-	/**
-	 * Contains the offset and length all the marked text. The dictionary is the
-	 * function: offset->length Marked text should be highlighted when
-	 * displayed. This Map should corrospond to the Excerption list (ie - the
-	 * forth element should point to the text of the forth excerption)
-	 */
+	/** Contains the offset and length all the marked text. The dictionary is the function: offset->length Marked text should be highlighted when displayed. This Map should corrospond to the Excerption list (ie - the forth element should point to the text of the forth excerption) */
 	SortedMap<Integer, Integer> markedText;
 
+	/**
+	 * Update excerption view.
+	 */
 	private void updateExcerptionView() {
 		ExcerptionView view = ExcerptionView.getView();
 		if (view == null) return;
@@ -159,8 +166,8 @@ public class OperationTable extends TextEditor {
 //	}
 	
 	/**
-	 * Updates the colored lines and the general display of the editor
-	 */
+ * Updates the colored lines and the general display of the editor.
+ */
 	public void refreshDisplay() {
 		if (rootDiscussionsView) {
 			rootDiscussions.refreshDisplay();
@@ -219,8 +226,9 @@ public class OperationTable extends TextEditor {
 		srcview.getTextWidget().redraw();
 	}
 
-	/** 
-	 * Adds the selected text to the marked text set
+	/**
+	 * Adds the selected text to the marked text set.
+	 * 
 	 * @param t the current selection, which should be added
 	 */
 	public void mark(TextSelection t) {
@@ -257,14 +265,11 @@ public class OperationTable extends TextEditor {
 
 	/**
 	 * Updates markedExcerptions and markedText The mark should be inside a
-	 * single ChapterText
+	 * single ChapterText.
 	 * 
-	 * @param offset
-	 *            the mark's offset inside the document
-	 * @param length
-	 *            the length of the mark
-	 * @param c
-	 *            the chapter text which contains the marking
+	 * @param offset the mark's offset inside the document
+	 * @param length the length of the mark
+	 * @param c the chapter text which contains the marking
 	 */
 	protected void markChapterExcerption(int offset, int length, ChapterText c) {
 		int mergedBegin = offset; // we will update this if we make a merge
@@ -314,21 +319,25 @@ public class OperationTable extends TextEditor {
 	}
 
 	/**
-	 * Returns the selected excerptions in the editor
+	 * Returns the selected excerptions in the editor.
+	 * 
+	 * @return the excerptions
 	 */
 	public SortedMap<Integer, Excerption> getExcerptions() {
 		return markedExcerptions;
 	}
 	
 	/**
-	 * Returns the selected excerptions in the editor
+	 * Returns the selected excerptions in the editor.
+	 * 
+	 * @return the marked
 	 */
 	public List<Excerption> getMarked() {
 		return new Vector<Excerption>(markedExcerptions.values());
 	}
 
 	/**
-	 * Clears all the marked excerptions from the editor
+	 * Clears all the marked excerptions from the editor.
 	 */
 	public void clearMarked() {
 		markedText.clear();
@@ -338,6 +347,8 @@ public class OperationTable extends TextEditor {
 
 	/**
 	 * Cuts text from the source. Currently is an Unsupported Operation
+	 * 
+	 * @param t the t
 	 */
 	public void cut(TextSelection t) {
 		throw new UnsupportedOperationException(
@@ -345,16 +356,19 @@ public class OperationTable extends TextEditor {
 	}
 
 	/**
-	 * Returns the Source Document displayed by the editor
+	 * Returns the Source Document displayed by the editor.
+	 * 
+	 * @return the document
 	 */
 	public SourceDocument getDocument() {
 		return (SourceDocument) getSourceViewer().getDocument();
 	}
 
+	/** The old source viewer configuration. */
 	private SourceViewerConfiguration oldSourceViewerConfiguration = null;
 
 	/**
-	 * Highlights the discussion links in the editor 
+	 * Highlights the discussion links in the editor.
 	 */
 	public void showDiscussions() {
 		if (oldSourceViewerConfiguration == null)
@@ -374,7 +388,7 @@ public class OperationTable extends TextEditor {
 	}
 	
 	/**
-	 * Hide the discussion links from the editor
+	 * Hide the discussion links from the editor.
 	 */
 	public void hideDisucssions() {
 		if (oldSourceViewerConfiguration != null)
@@ -391,7 +405,9 @@ public class OperationTable extends TextEditor {
 	}
 
 	/**
-	 * Returns the project associated with the currently displayed file
+	 * Returns the project associated with the currently displayed file.
+	 * 
+	 * @return the project
 	 */
 	public IProject getProject() {
 		FileEditorInput fileEditorInput = 
@@ -401,11 +417,13 @@ public class OperationTable extends TextEditor {
 	}
 
 	/**
-	 * Removes a specific excerptions from the marked text
+	 * Removes a specific excerptions from the marked text.
+	 * 
 	 * @param i the index of the excerption (in terms of start offset in the text)
 	 */
 	public void removeExcerption(int i) {
 		markedExcerptions.remove(i);
 		markedText.remove(i);
 	}
+	
 }
