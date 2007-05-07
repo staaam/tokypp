8b25c375184d6323e249d2c34b02ef4c4b898905
shynahum <shynahum@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-05-07 14:26:43 +0000

    Type: Testing Framework What: Removing External Files (incomplete) Description: Added a method for loading an input stream of a file inside the plugin Haven't checked yet that it is working

--- trunk/YPPluginTests/lost/tok/Paths.java
+++ trunk/YPPluginTests/lost/tok/Paths.java
@@ -1,9 +1,20 @@
 package lost.tok;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
 /**
  * Contains the paths to all the used test files
  */
 public class Paths {
+	
+	/** The name of the tests plug-in */
+	private static final String BUNDLE_NAME = "YPPluginTests";
 
 	/** A friendly message for FileNotFound */
 	static public final String FILE_NOT_FOUND_MESSAGE = "File Not Found. Please create a directory called C:/Temp/TestFiles/ and copy the test files to it";
@@ -28,5 +39,17 @@ public class Paths {
 	final static public String OR_AHAIM_HE_RES = BASE + "Or_Ahaim_he.res";
 
 	final static public String SHORT_AHAIM_EN_RES = BASE + "Short_Ahaim_en.res";
+	
+	/**
+	 * Returns an input stream of an internal test file
+	 * @param filePath the path to the file in the project's files
+	 * @return input stream of the file
+	 * @throws IOException if opening fails
+	 */
+	public InputStream getInputStream(String filePath) throws IOException{
+		Path path = new Path(filePath);
+        Bundle bundle = Platform.getBundle(BUNDLE_NAME);
 
+        return FileLocator.openStream(bundle, path, false);
+   }
 }
