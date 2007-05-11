d46b9ebb1f5bb99c1354214b7b7db5f7daff63c6
arie.teter <arie.teter@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-05-11 22:59:16 +0000

    Type: New Feature What: Export discussions Wizard and supporting classes Description: Export discussion wizard, export to zip file

---
+++ YearlyPluginProject/src/lost/tok/wizards/ExportDiscussion.java
@@ -0,0 +1,71 @@
+package lost.tok.wizards;
+
+import java.util.List;
+
+import lost.tok.ToK;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.IWizardContainer;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IExportWizard;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.wizards.datatransfer.DataTransferMessages;
+import org.eclipse.ui.internal.wizards.datatransfer.WizardArchiveFileResourceExportPage1;
+import org.eclipse.ui.wizards.datatransfer.ZipFileExportWizard;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ExportDiscussion.
+ */
+public class ExportDiscussion extends ZipFileExportWizard
+ {
+
+	//private ExportDiscussionPage page;
+
+	/** The selection. */
+	private ISelection selection;
+
+	/** The tok. */
+	private ToK tok;
+
+	/** The project. */
+	private IProject project = null;
+	
+	private ExportDiscussionPage exportPage;
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
+		this.selection = currentSelection;
+        List selectedResources = IDE.computeSelectedResources(currentSelection);
+        if (!selectedResources.isEmpty()) {
+            this.selection = new StructuredSelection(selectedResources);
+        }
+
+        setDefaultPageImageDescriptor(IDEWorkbenchPlugin.getIDEImageDescriptor("wizban/exportzip_wiz.png"));//$NON-NLS-1$
+        setNeedsProgressMonitor(true);
+		setWindowTitle("Export Discussions");
+	}
+
+	@Override
+	public void addPages() {
+		//super.addPages();
+		exportPage = new ExportDiscussionPage((IStructuredSelection) selection);
+		addPage(exportPage);
+	}
+	
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return exportPage.finish();
+	}
+	
+}
