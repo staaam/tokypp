ef337a202d44e56c691aaa0c83d6a27669070581
arie.teter <arie.teter@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-05-13 22:05:43 +0000

    Type: New Feature What: Import discussions Wizard and supporting classes Description: Import discussion wizard, export to zip file

--- YearlyPluginProject/src/lost/tok/wizards/ImportDiscussion.java
+++ YearlyPluginProject/src/lost/tok/wizards/ImportDiscussion.java
@@ -10,46 +10,71 @@
  *******************************************************************************/
 package lost.tok.wizards;
 
-import org.eclipse.core.resources.IFile;
+import java.util.List;
+
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.ui.IImportWizard;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.internal.wizards.datatransfer.DataTransferMessages;
 import org.eclipse.ui.wizards.datatransfer.ZipFileImportWizard;
 
 public class ImportDiscussion extends ZipFileImportWizard {
-	
-//	ImportDiscussionPage mainPage;
-//
-//	public ImportDiscussion() {
-//		super();
-//	}
-//
-//	/* (non-Javadoc)
-//	 * @see org.eclipse.jface.wizard.Wizard#performFinish()
-//	 */
-//	public boolean performFinish() {
-//		IFile file = mainPage.createNewFile();
-//        if (file == null)
-//            return false;
-//        return true;
-//	}
-//	 
-//	/* (non-Javadoc)
-//	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
-//	 */
-//	public void init(IWorkbench workbench, IStructuredSelection selection) {
-//		setWindowTitle("Discussions Import Wizard"); //NON-NLS-1
-//		setNeedsProgressMonitor(true);
-//		mainPage = new ImportDiscussionPage("Import Discussions",selection); //NON-NLS-1
-//	}
-//	
-//	/* (non-Javadoc)
-//     * @see org.eclipse.jface.wizard.IWizard#addPages()
-//     */
-//    public void addPages() {
-//        super.addPages(); 
-//        addPage(mainPage);        
-//    }
 
+	private ImportDiscussionPage importPage;
+
+	private IWorkbench workbench;
+
+	private IStructuredSelection selection;
+
+	public ImportDiscussion() {
+		super();
+	}
+
+	 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+	 *      org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
+		this.workbench = workbench;
+		this.selection = currentSelection;
+		List selectedResources = IDE.computeSelectedResources(currentSelection);
+		if (!selectedResources.isEmpty()) {
+			this.selection = new StructuredSelection(selectedResources);
+		}
+
+		setDefaultPageImageDescriptor(IDEWorkbenchPlugin
+				.getIDEImageDescriptor("wizban/importzip_wiz.png"));//$NON-NLS-1$
+		setNeedsProgressMonitor(true);
+		setWindowTitle("Discussions Import Wizard"); // NON-NLS-1
+		setNeedsProgressMonitor(true);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.IWizard#addPages()
+	 */
+	public void addPages() {
+		importPage = new ImportDiscussionPage(workbench, selection); // NON-NLS-1
+		addPage(importPage);
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWizard.
+	 */
+	public boolean performCancel() {
+		return importPage.cancel();
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWizard.
+	 */
+	public boolean performFinish() {
+		return importPage.finish();
+	}
 }
