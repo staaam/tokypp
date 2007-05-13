800dc69dd7e7e06aef7b0c634fd7cdc1c7671123
shynahum <shynahum@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-05-13 22:29:21 +0000

    Type: Code Cleanup What: Removed a few warnings Description: Solved a few trivial warnings

--- trunk/YPPluginTests/lost/tok/opTable/sourceDocument/SourceDocumentTest.java
+++ trunk/YPPluginTests/lost/tok/opTable/sourceDocument/SourceDocumentTest.java
@@ -1,7 +1,6 @@
 package lost.tok.opTable.sourceDocument;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import junit.framework.TestCase;
