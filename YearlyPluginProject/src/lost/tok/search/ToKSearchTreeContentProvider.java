0308039b17a8d60f831d277190b8bb177cded7f6
michael.gelfand <michael.gelfand@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-01 16:20:50 +0000

    Type: New Feature What: 13.1 - tab added to search dialog Description: Tab 'ToK Search' added to search dialog, it displays possible options for searching + stubs of other search related classes

---
+++ YearlyPluginProject/src/lost/tok/search/ToKSearchTreeContentProvider.java
@@ -0,0 +1,50 @@
+package lost.tok.search;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.search.internal.ui.text.IFileSearchContentProvider;
+
+public class ToKSearchTreeContentProvider implements ITreeContentProvider,
+		IFileSearchContentProvider {
+
+	public Object[] getChildren(Object parentElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getParent(Object element) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasChildren(Object element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object[] getElements(Object inputElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void elementsChanged(Object[] updatedElements) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
