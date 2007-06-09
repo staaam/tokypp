f8bc9961c72ab49fd909eb30ecbc54c6fd55d891
evgeni.dubov <evgeni.dubov@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-09 17:23:36 +0000

    Type: New Feature What: New file which represent author element Description: 	Handles all author related functions

--- YearlyPluginProject/src/lost/tok/Author.java
+++ YearlyPluginProject/src/lost/tok/Author.java
@@ -1,16 +1,78 @@
-/**
- * 
- */
 package lost.tok;
 
-/**
- * @author Team LOST
- * 
- */
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+
 public class Author {
-	// TODO: Create enum for author ranks (where??)
+
+	/**********************************************************************
+	 * M E M B E R S
+	 **********************************************************************/
+	
+	private Integer ID;
 
 	public String name;
+	public Integer rank;
 
-	public int rank;
+	
+	/**********************************************************************
+	 * C ' T O R
+	 **********************************************************************/
+
+	public Author(Integer authRank, String authName) {
+		name = authName;
+		rank = authRank;
+		ID = Math.abs(hashCode());
+	}
+
+	public Author() {}
+	
+	/**********************************************************************
+	 * P R O P E R T I E S
+	 **********************************************************************/
+	
+	public void setID(Integer authID) {
+		ID = authID;
+	}
+
+	public Integer getID() {
+		return ID;
+	}
+
+	public void setRank(Integer authRank) {
+		rank = authRank;
+	}
+
+	public Integer getRank() {
+		return rank;
+	}
+	
+	public void setName(String authName) {
+		name = authName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	
+	/**********************************************************************
+	 * P U B L I C   M E T H O D S
+	 **********************************************************************/
+	
+	/**
+	 * Creating an author XML element
+	 * 
+	 * @return Element of Author
+	 */
+	public Element toXML() {
+		Element e = DocumentHelper.createElement("author");
+		e.addText(getName());
+		
+		return e;
+	}
+	
+	public int hashCode() {
+		return 57 * name.hashCode();
+	}
 }
