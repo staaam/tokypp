dfefe5ff1ddcd075758314de8f06a1e6085b1bef
guy.hareuveni <guy.hareuveni@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-26 17:36:54 +0000

    Type: New features What: Import sources and Export sources Policy failures: Code warning - failed on resource SourceExportOperation.java. Reason: The field SourceExportOperation.tok is never read locally, line 5 Override reason: Minor warning, will be fixed

---
+++ trunk/YearlyPluginProject/src/lost/tok/export/SourceZipFileExporter.java
@@ -0,0 +1,139 @@
+package lost.tok.export;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.zip.CRC32;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.ui.internal.wizards.datatransfer.IFileExporter;
+
+/**
+ * Exports resources to a .zip file
+ */
+
+public class SourceZipFileExporter implements IFileExporter {
+
+	/** The output stream. */
+	private ZipOutputStream outputStream;
+
+	/** The use compression. */
+	private boolean useCompression = true;
+
+	/**
+	 * Create an instance of this class.
+	 * 
+	 * @param filename
+	 *            java.lang.String
+	 * @param compress
+	 *            boolean
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * 
+	 * @exception java.io.IOException
+	 */
+	public SourceZipFileExporter(String filename, boolean compress)
+			throws IOException {
+		outputStream = new ZipOutputStream(new FileOutputStream(filename));
+		useCompression = compress;
+	}
+
+	/**
+	 * Do all required cleanup now that we're finished with the currently-open
+	 * .zip
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * 
+	 * @exception java.io.IOException
+	 */
+	public void finished() throws IOException {
+		outputStream.close();
+	}
+
+	/**
+	 * Write the contents of the file to the tar archive.
+	 * 
+	 * @param entry
+	 *            the entry
+	 * @param contents
+	 *            the contents
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * @throws CoreException
+	 *             the core exception
+	 * 
+	 * @exception java.io.IOException
+	 * @exception org.eclipse.core.runtime.CoreException
+	 */
+	private void write(ZipEntry entry, IFile contents) throws IOException,
+			CoreException {
+		byte[] readBuffer = new byte[4096];
+
+		// If the contents are being compressed then we get the below for free.
+		if (!useCompression) {
+			entry.setMethod(ZipEntry.STORED);
+			InputStream contentStream = contents.getContents(false);
+			int length = 0;
+			CRC32 checksumCalculator = new CRC32();
+			try {
+				int n;
+				while ((n = contentStream.read(readBuffer)) > 0) {
+					checksumCalculator.update(readBuffer, 0, n);
+					length += n;
+				}
+			} finally {
+				if (contentStream != null) {
+					contentStream.close();
+				}
+			}
+
+			entry.setSize(length);
+			entry.setCrc(checksumCalculator.getValue());
+		}
+
+		outputStream.putNextEntry(entry);
+		InputStream contentStream = contents.getContents(false);
+		try {
+			int n;
+			while ((n = contentStream.read(readBuffer)) > 0) {
+				outputStream.write(readBuffer, 0, n);
+			}
+		} finally {
+			if (contentStream != null) {
+				contentStream.close();
+			}
+		}
+		outputStream.closeEntry();
+	}
+
+	/**
+	 * Write the passed resource to the current archive.
+	 * 
+	 * @param resource
+	 *            org.eclipse.core.resources.IFile
+	 * @param destinationPath
+	 *            java.lang.String
+	 * 
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 * @throws CoreException
+	 *             the core exception
+	 * 
+	 * @exception java.io.IOException
+	 * @exception org.eclipse.core.runtime.CoreException
+	 */
+	public void write(IFile resource, String destinationPath)
+			throws IOException, CoreException {
+		ZipEntry newEntry = new ZipEntry(destinationPath);
+		String fileName = resource.getName();
+		String[] name = fileName.split("\\."); //$NON-NLS-1$
+		newEntry.setComment(name[0]);
+		write(newEntry, resource);
+	}
+}
---
+++ trunk/YearlyPluginProject/src/lost/tok/wizards/ImportSource.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package lost.tok.wizards;
+
+import java.util.List;
+
+import lost.tok.Messages;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.eclipse.ui.wizards.datatransfer.ZipFileImportWizard;
+
+/**
+ * The Class ImportSource.
+ */
+public class ImportSource extends ZipFileImportWizard {
+
+	/** The import page. */
+	private ImportSourcePage importPage;
+
+	/** The workbench. */
+	private IWorkbench workbench;
+
+	/** The selection. */
+	private IStructuredSelection selection;
+
+	/**
+	 * Instantiates a new import discussion.
+	 */
+	public ImportSource() {
+		super();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench,
+	 *      org.eclipse.jface.viewers.IStructuredSelection)
+	 */
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection currentSelection) {
+		this.workbench = workbench;
+		this.selection = currentSelection;
+		List selectedResources = IDE.computeSelectedResources(currentSelection);
+		if (!selectedResources.isEmpty()) {
+			this.selection = new StructuredSelection(selectedResources);
+		}
+
+		setDefaultPageImageDescriptor(IDEWorkbenchPlugin
+				.getIDEImageDescriptor("wizban/importzip_wiz.png"));//$NON-NLS-1$
+		setNeedsProgressMonitor(true);
+		setWindowTitle(Messages.getString("ImportSource.0")); // NON-NLS-1
+																	// //$NON-NLS-1$
+		setNeedsProgressMonitor(true);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.wizard.IWizard#addPages()
+	 */
+	@Override
+	public void addPages() {
+		importPage = new ImportSourcePage(workbench, selection); // NON-NLS-1
+		addPage(importPage);
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWizard.
+	 */
+	@Override
+	public boolean performCancel() {
+		return importPage.cancel();
+	}
+
+	/*
+	 * (non-Javadoc) Method declared on IWizard.
+	 */
+	@Override
+	public boolean performFinish() {
+		return importPage.finish();
+	}
+}
