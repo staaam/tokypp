9ca34d7b966c423809f78ee30996a4002e996442
guy.hareuveni <guy.hareuveni@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-27 15:12:58 +0000

    Type: New feature What: a new navigator does nothing special at this point

---
+++ YearlyPluginProject/src/lost/tok/navigator/InformationComparator.java
@@ -0,0 +1,12 @@
+package lost.tok.navigator;
+
+import java.util.Comparator;
+
+public class InformationComparator implements Comparator{
+
+	public int compare(Object arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
