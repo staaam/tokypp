e6838d4af5a2806029ffc00af207b4454c2b938e
evgeni.dubov <evgeni.dubov@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-27 22:17:41 +0000

    Type: Addition What: Code refinment Description: 	Code refinment

--- trunk/YearlyPluginProject/src/lost/tok/Rank.java
+++ trunk/YearlyPluginProject/src/lost/tok/Rank.java
@@ -2,46 +2,67 @@ package lost.tok;
 
 import org.dom4j.Element;
 
+/**
+ * Contains all data related to authors rank
+ * @author evgeni
+ *
+ */
 public class Rank {
-	
-	/**********************************************************************
-	 * M E M B E R S
-	 **********************************************************************/
-	
+
 	String name;
 	int id;
 
-	
-	/**********************************************************************
-	 * C ' T O R S
-	 **********************************************************************/
-	
+	/**
+	 * Rank class construcor
+	 * @param rankName
+	 * @param rankId
+	 */
 	public Rank(String rankName, int rankId) {
 		name = rankName;
 		id = rankId;
 	}
 
+	/**
+	 * Rank class construcor
+	 * @param rankName
+	 * @param rankId
+	 */
 	public Rank(Element e) {
 		this(e.element("name").getText(), Integer.valueOf(e.element("id").getText()));
 	}
 
-	
-	/**********************************************************************
-	 * P R O P E R T I E S
-	 **********************************************************************/
-	
+
+	/**
+	 * Get rank ID
+	 * @return
+	 */
 	public int getId() {
 		return id;
 	}
 
+	
+	/**
+	 * Set rank ID
+	 * @param rankId
+	 */
 	public void setId(int rankId) {
 		id = rankId;
 	}
 
+	
+	/**
+	 * Get rank name
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
 
+	
+	/**
+	 * Set rank name
+	 * @param rankId
+	 */
 	public void setName(String rankName) {
 		name = rankName;
 	}
