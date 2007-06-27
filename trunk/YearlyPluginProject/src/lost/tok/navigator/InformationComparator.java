38b1886addc8df8edb53eff7ea38c7e37ef13adf
guy.hareuveni <guy.hareuveni@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-27 16:38:47 +0000

    for self use only Policy failures: Code warning - failed on resource InformationComparator.java. Reason: The import java.util.Comparator is never used, line 3 - failed on resource InformationNavigator.java. Reason: The import java.util.Comparator is never used, line 3 - failed on resource InformationComparator.java. Reason: The import org.eclipse.core.runtime.IPath is never used, line 6 - failed on resource InformationNavigator.java. Reason: The import org.eclipse.jface.viewers.ViewerComparator is never used, line 5 - failed on resource InformationComparator.java. Reason: The local variable r1 is never read, line 29 ... and more. Override reason: _

--- trunk/YearlyPluginProject/src/lost/tok/navigator/InformationComparator.java
+++ trunk/YearlyPluginProject/src/lost/tok/navigator/InformationComparator.java
@@ -2,11 +2,34 @@ package lost.tok.navigator;
 
 import java.util.Comparator;
 
-public class InformationComparator implements Comparator{
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.views.navigator.ResourceSorter;
 
-	public int compare(Object arg0, Object arg1) {
-		// TODO Auto-generated method stub
-		return 0;
+
+public class InformationComparator extends ResourceSorter{
+	
+	public InformationComparator(int criteria) {
+		super(criteria);
+	}
+	
+
+
+    /* (non-Javadoc)
+     * Method declared on ViewerSorter.
+     */
+	public int compare(Viewer viewer, Object o1, Object o2) {
+		 //have to deal with non-resources in navigator
+        //if one or both objects are not resources, returned a comparison 
+        //based on class.
+        if (!(o1 instanceof IResource && o2 instanceof IResource)) {
+            return compareClass(o1, o2);
+        }
+        IResource r1 = (IResource) o1;
+        IResource r2 = (IResource) o2;
+
+        return -super.compare(viewer, o1, o2);
 	}
 
 }
--- trunk/YearlyPluginProject/src/lost/tok/navigator/InformationNavigator.java
+++ trunk/YearlyPluginProject/src/lost/tok/navigator/InformationNavigator.java
@@ -1,7 +1,18 @@
 package lost.tok.navigator;
 
+import java.util.Comparator;
+
+import org.eclipse.jface.viewers.ViewerComparator;
 import org.eclipse.ui.views.navigator.*;
 
 public class InformationNavigator extends ResourceNavigator{
+
+	public InformationNavigator() {
+		super();
+		//ResourceSorter sorter = (new InformationComparator());
+		//this.setSorter(sorter);
+	}
+	
+	
 	
 }
