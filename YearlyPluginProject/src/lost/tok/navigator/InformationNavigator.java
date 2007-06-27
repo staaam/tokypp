36ca51d1f79fe64776de2394f2ea32ea7b6557a1
michalzim <michalzim@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-27 18:07:44 +0000

    type: feature what: added an ID for the sake of the perspective

--- YearlyPluginProject/src/lost/tok/navigator/InformationNavigator.java
+++ YearlyPluginProject/src/lost/tok/navigator/InformationNavigator.java
@@ -7,7 +7,7 @@ import org.eclipse.ui.views.navigator.*;
 
 public class InformationNavigator extends ResourceNavigator{
 	
-	public final static String ID = "lost.tok.navigator.InformationNavigator"; //$NON-NLS-1$
+	public final static String ID = "lost.tok.navigator.Navigator"; //$NON-NLS-1$
 
 	public InformationNavigator() {
 		super();
