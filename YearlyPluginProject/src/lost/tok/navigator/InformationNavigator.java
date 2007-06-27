924b4e19c35c0e59a04839f7f9048a8de27dd066
guy.hareuveni <guy.hareuveni@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-27 18:22:45 +0000

    Type: Feature fix What: ToK Navigator Description: 	ToK Navigator order is now the navigator order in reverse

--- YearlyPluginProject/src/lost/tok/navigator/InformationNavigator.java
+++ YearlyPluginProject/src/lost/tok/navigator/InformationNavigator.java
@@ -2,7 +2,10 @@ package lost.tok.navigator;
 
 import java.util.Comparator;
 
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.ViewerComparator;
+import org.eclipse.jface.viewers.ViewerSorter;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.views.navigator.*;
 
 public class InformationNavigator extends ResourceNavigator{
@@ -11,10 +14,14 @@ public class InformationNavigator extends ResourceNavigator{
 
 	public InformationNavigator() {
 		super();
-		//ResourceSorter sorter = (new InformationComparator());
-		//this.setSorter(sorter);
 	}
 	
+	
+	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+		ResourceSorter sorter = new InformationComparator();
+		super.setSorter(sorter);
+	}
 	
 	
 }
