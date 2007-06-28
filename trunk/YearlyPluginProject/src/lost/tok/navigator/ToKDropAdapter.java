65da7c6878544ab342f618e2b25f84a4d0879991
guy.hareuveni <guy.hareuveni@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-06-28 13:12:14 +0000

    Type: New feature What: InformaionNavigator Description: place for all the things I need to do. Policy failures: Code warning - failed on resource InformationNavigator.java. Reason: The import org.eclipse.jface.util.DelegatingDropAdapter is never used, line 3 - failed on resource InformationNavigator.java. Reason: The import org.eclipse.jface.viewers.ISelection is never used, line 4 - failed on resource InformationNavigator.java. Reason: The import org.eclipse.jface.viewers.IStructuredSelection is never used, line 5 - failed on resource InformationNavigator.java. Reason: The import org.eclipse.swt.dnd.DropTargetEvent is never used, line 10 - failed on resource InformationNavigator.java. Reason: The import org.eclipse.swt.dnd.DropTargetListener is never used, line 11 ... and more. Override reason: _

---
+++ trunk/YearlyPluginProject/src/lost/tok/navigator/ToKDropAdapter.java
@@ -0,0 +1,18 @@
+package lost.tok.navigator;
+
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.ui.views.navigator.NavigatorDropAdapter;
+
+public class ToKDropAdapter extends NavigatorDropAdapter{
+
+	public ToKDropAdapter(StructuredViewer viewer) {
+		super(viewer);
+	}
+	
+	public boolean performDrop(final Object data) {
+		
+		return super.performDrop(data);
+	}
+	
+
+}
