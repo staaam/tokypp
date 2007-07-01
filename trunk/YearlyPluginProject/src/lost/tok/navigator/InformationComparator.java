facb478d46bc6b9596efb135a903f2f89c127247
guy.hareuveni <guy.hareuveni@00bcd634-1527-0410-a43b-811fbd66bea9>
2007-07-01 07:58:44 +0000

    Type: New feature What: Information Navigator Description: now show sources in the right order Policy failures: Code warning - failed on resource InformationComparator.java. Reason: The import org.eclipse.core.resources.IProject is never used, line 14 - failed on resource InformationComparator.java. Reason: The import org.eclipse.core.runtime.IPath is never used, line 1 Override reason: _

--- trunk/YearlyPluginProject/src/lost/tok/navigator/InformationComparator.java
+++ trunk/YearlyPluginProject/src/lost/tok/navigator/InformationComparator.java
@@ -1,5 +1,19 @@
 package lost.tok.navigator;
 
+import java.util.List;
+
+import lost.tok.GeneralFunctions;
+import lost.tok.ToK;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Node;
+import org.dom4j.XPath;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.ui.views.navigator.ResourceSorter;
 
@@ -16,29 +30,41 @@ public class InformationComparator extends ResourceSorter{
      * Method declared on ViewerSorter.
      */
 	public int compare(Viewer viewer, Object o1, Object o2) {
-		 //have to deal with non-resources in navigator
+		//have to deal with non-resources in navigator
         //if one or both objects are not resources, returned a comparison 
         //based on class.
-//        if (!(o1 instanceof IResource && o2 instanceof IResource)) {
-//            return compareClass(o1, o2);
-//        }
-//        IResource r1 = (IResource) o1;
-//        IResource r2 = (IResource) o2;
-//        IProject project = r1.getProject();
-//        String sor = ToK.getProjectToK(project).getSourcesFolder().getFullPath().toString();
-//        String rot = ToK.getProjectToK(project).getRootsFolder().getFullPath().toString();
-//        String s1 = r1.getFullPath().toString();
-//        String s2 = r2.getFullPath().toString();
-//        
-//        if ((s1.startsWith(sor) && s2.startsWith(sor)) || (s1.startsWith(rot) && s2.startsWith(rot))){
-//        	IFile order = (IFile) r1.getParent().findMember("order.xml");
-//        	Document d = GeneralFunctions.readFromXML(order);
-//        	XPath xpathSelector = DocumentHelper.createXPath("");
-//    		List result = xpathSelector. .selectNodes(d);
-//        	return super.compare(viewer, o1, o2);
-//        }
+        if (!(o1 instanceof IResource && o2 instanceof IResource)) {
+            return compareClass(o1, o2);
+        }
+        IResource r1 = (IResource) o1;
+        IResource r2 = (IResource) o2;
+        String l1 = r1.getFullPath().toString();
+        l1 = l1.substring(0, l1.lastIndexOf('/'));
+        String l2 = r2.getFullPath().toString();
+        l2 = l2.substring(0, l2.lastIndexOf('/'));
+        if (!((l1.contains(ToK.SOURCES_FOLDER) && l2.contains(ToK.SOURCES_FOLDER)) || (l1.contains(ToK.ROOTS_FOLDER) && l2.contains(ToK.ROOTS_FOLDER))) || l1.contains(ToK.UNPARSED_SOURCES_FOLDER)){
+        	return super.compare(viewer, o1, o2);
+        }
+        
+        IFolder f = (IFolder) r1.getParent();
+        IFile file = f.getFile("order.xml");
+        Document d = GeneralFunctions.readFromXML(file);
+        
+        String s1 = r1.getName();
+        String s2 = r2.getName();
+        
+        XPath xpathSelector = DocumentHelper.createXPath("//sub[name='" + s1 + "']");
+		Node node1 = xpathSelector.selectSingleNode(d);
+        
+        xpathSelector = DocumentHelper.createXPath("//sub[name='" + s2 + "']");
+		Node node2 = xpathSelector.selectSingleNode(d);
+               
+        xpathSelector = DocumentHelper.createXPath("//sub");
+        List list = xpathSelector.selectNodes(d);
+        int i1 = list.indexOf(node1);
+        int i2 = list.indexOf(node2);
 
-        return super.compare(viewer, o1, o2);
+        return i1 - i2;
 	}
 
 }
